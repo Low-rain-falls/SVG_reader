cd62847d7d3da06cca6be136cecd20a1d4ef37a9
tranlynhathao <haotran04022005@gmail.com>
2024-10-27 13:28:25 +0700

    add readme: checklist

--- Line.h
+++ Line.h
@@ -1,50 +1,51 @@
 #pragma once
 
-#include <objidl.h>
-#include <gdiplus.h>
 #include "stdafx.h"
+#include <gdiplus.h>
+#include <objidl.h>
 
 using namespace Gdiplus;
 
-//<line x1 = "10" y1 = "30" x2 = "30" y2 = "10" stroke = "rgb(0, 0, 255)" stroke - width = "5" stroke - opacity = "0.7" / >
-//<line x1 = "30" y1 = "30" x2 = "50" y2 = "10" stroke = "rgb(0, 0, 255)" stroke - width = "10" stroke - opacity = "0.8" / >
-//<line x1 = "50" y1 = "30" x2 = "70" y2 = "10" stroke = "rgb(0, 0, 255)" stroke - width = "15" stroke - opacity = "0.9" / >
-//<line x1 = "70" y1 = "30" x2 = "90" y2 = "10" stroke = "rgb(0, 0, 255)" stroke - width = "20" stroke - opacity = "0.9" / >
-//<line x1 = "90" y1 = "30" x2 = "110" y2 = "10" stroke = "rgb(0, 0, 255)" stroke - width = "25" stroke - opacity = "1.0" / >
+//<line x1 = "10" y1 = "30" x2 = "30" y2 = "10" stroke = "rgb(0, 0, 255)" stroke
+//- width = "5" stroke - opacity = "0.7" / > <line x1 = "30" y1 = "30" x2 = "50"
+// y2 = "10" stroke = "rgb(0, 0, 255)" stroke - width = "10" stroke - opacity =
+//"0.8" / > <line x1 = "50" y1 = "30" x2 = "70" y2 = "10" stroke = "rgb(0, 0,
+// 255)" stroke - width = "15" stroke - opacity = "0.9" / > <line x1 = "70" y1 =
+//"30" x2 = "90" y2 = "10" stroke = "rgb(0, 0, 255)" stroke - width = "20"
+// stroke - opacity = "0.9" / > <line x1 = "90" y1 = "30" x2 = "110" y2 = "10"
+// stroke = "rgb(0, 0, 255)" stroke - width = "25" stroke - opacity = "1.0" / >
 
-class myLine
-{
+class myLine {
 private:
-	int x1;
-	int y1;
-	int x2;
-	int y2;
-	Color stroke;
-	int strokeWidth;
-public:
-	myLine(int, int, int, int, Color, int, float);
-	~myLine();
-	void Draw(Graphics graphics);
+  int x1;
+  int y1;
+  int x2;
+  int y2;
+  Color stroke;
+  int strokeWidth;
 
+public:
+  myLine(int, int, int, int, Color, int, float);
+  ~myLine();
+  void Draw(Graphics graphics);
 };
 
-myLine::myLine(int x1, int y1, int x2, int y2, Color stroke, int strokeWidth, float strokeOpacity)
-{
-	this->x1 = x1;
-	this->y1 = y1;
-	this->x2 = x2;
-	this->y2 = y2;
-	//this->stroke.SetValue(Color(strokeOpacity * 255, stroke.GetR(), stroke.GetG(), stroke.GetB()));
-	this->stroke = Color(strokeOpacity * 255, stroke.GetR(), stroke.GetG(), stroke.GetB());	
-	this->strokeWidth = strokeWidth;
+myLine::myLine(int x1, int y1, int x2, int y2, Color stroke, int strokeWidth,
+               float strokeOpacity) {
+  this->x1 = x1;
+  this->y1 = y1;
+  this->x2 = x2;
+  this->y2 = y2;
+  // this->stroke.SetValue(Color(strokeOpacity * 255, stroke.GetR(),
+  // stroke.GetG(), stroke.GetB()));
+  this->stroke =
+      Color(strokeOpacity * 255, stroke.GetR(), stroke.GetG(), stroke.GetB());
+  this->strokeWidth = strokeWidth;
 }
 
-void myLine::Draw(Graphics graphics)
-{
-	Pen strokeDraw(this->stroke, this->strokeWidth);
-	graphics.DrawLine(&strokeDraw, this->x1, this->y1, this->x2, this->y2);
+void myLine::Draw(Graphics graphics) {
+  Pen strokeDraw(this->stroke, this->strokeWidth);
+  graphics.DrawLine(&strokeDraw, this->x1, this->y1, this->x2, this->y2);
 }
 
-myLine::~myLine()
-{
-}
+myLine::~myLine() {}
