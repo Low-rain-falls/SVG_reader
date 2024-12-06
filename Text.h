e6be4894c5097c829481837f10a3c6fae2a9a06c
PikaPuu <145943198+thanhdatttt@users.noreply.github.com>
2024-12-06 14:42:14 +0700

    Update Text

--- Text.h
+++ Text.h
@@ -22,16 +22,41 @@ public:
 
 	void render(Graphics& graphics) override
 	{
-		FontFamily fontFamily(L"Consolas");
-		Font font(&fontFamily, static_cast<REAL>(font_size), FontStyleRegular, UnitPixel);
+		FontFamily fontFamily(std::wstring(font_family.begin(), font_family.end()).c_str());
+		FontStyle style = (font_style == "italic") ? FontStyleItalic : FontStyleRegular;
+		Font font(&fontFamily, static_cast<REAL>(font_size), style, UnitPixel);
 		SolidBrush brush(fill);
-		PointF pointF(static_cast<REAL>(x), static_cast<REAL>(y));
+		
+		RectF boundingBox;
+		graphics.MeasureString(
+			std::wstring(content.begin(), content.end()).c_str(),
+			-1,
+			&font,
+			PointF(0, 0),
+			&boundingBox
+		);
+		
+		REAL text_width = boundingBox.Width;
+		
+		REAL adjusted_x = static_cast<REAL>(x);
+		if (text_anchor == "middle")
+		{
+			adjusted_x -= text_width / 2; // C?n gi?a
+		}
+		else if (text_anchor == "end")
+		{
+			adjusted_x -= text_width; // C?n ph?i
+		}
+		
+		PointF pointF(static_cast<REAL>(adjusted_x), static_cast<REAL>(y));
 		graphics.DrawString(std::wstring(content.begin(), content.end()).c_str(), -1, &font, pointF, &brush);
-		Pen pen(stroke, stroke_width);
-		GraphicsPath path;
-		StringFormat format;
-		path.AddString(std::wstring(content.begin(), content.end()).c_str(), -1, &fontFamily, FontStyleRegular, static_cast<REAL>(font_size), pointF, &format);
-		graphics.DrawPath(&pen, &path);
+		if (stroke_width != 0){
+			Pen pen(stroke, stroke_width);
+			GraphicsPath path;
+			StringFormat format;
+			path.AddString(std::wstring(content.begin(), content.end()).c_str(), -1, &fontFamily, style, static_cast<REAL>(font_size), pointF, &format);
+			graphics.DrawPath(&pen, &path);
+		}
 	}
 
 	~my_text() override {
