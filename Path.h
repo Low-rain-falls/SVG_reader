198be722ad8651b4be2560c849e675b0af669827
Hownameee <namhaohuynh@gmail.com>
2024-12-06 14:14:18 +0700

    update path

--- Path.h
+++ Path.h
@@ -3,132 +3,129 @@
 #define PATH_H
 
 #include "SVGElement.h"
+#include <vector>
 #include "stdafx.h"
 #include <gdiplus.h>
 #include <objidl.h>
-#include <vector>
+#include <tuple>
 
 using namespace Gdiplus;
 using namespace std;
 
-//<path fill = "none" stroke = "rgb(255,0,0)" stroke - width = "5" d = "M100,200
-// C100,100 250,100 250,200 C250,300 400,300 400,200" / >
+//<path fill = "none" stroke = "rgb(255,0,0)" stroke - width = "5" d = "M100,200 C100,100 250,100 250,200 C250,300 400,300 400,200" / >
 
-class my_path : public SVGElement {
+class my_path : public SVGElement
+{
 private:
-  vector<char> commands;
-  int num_points;
-  double *points;
-  Color fill_color;
-  double fill_opacity;
-  Color stroke_color;
-  double stroke_width;
-  string transform;
-
+	vector<tuple<char, vector<PointF>>> d;
+	Color fill_color;
+	double fill_opacity;
+	Color stroke_color;
+	double stroke_width;
+	string transform;
 public:
-  my_path(string name, string transform, vector<char> commands, double *points,
-          int num_points, Color fill_color, double fill_opacity,
-          Color stroke_color, double stroke_width);
-  void render(Graphics &graphics) override;
-  string getTransform() override { return transform; }
+	my_path(string name, string transform, vector<tuple<char, vector<PointF>>> d, Color fill_color, double, Color stroke_color, double stroke_width);
+	void render(Graphics& graphics) override;
+	string getTransform() override { return transform; }
 };
 
-my_path::my_path(string name, string transform, vector<char> commands,
-                 double *points, int num_points, Color fill_color,
-                 double fill_opacity, Color stroke_color, double stroke_width)
-    : commands(commands), points(points), num_points(num_points),
-      fill_color(fill_color), stroke_color(stroke_color),
-      stroke_width(stroke_width), fill_opacity(fill_opacity), SVGElement(name),
-      transform(transform) {}
+my_path::my_path(string name, string transform, vector<tuple<char, vector<PointF>>> d, Color fill_color, double fill_opacity, Color stroke_color, double stroke_width = 0) : d(d), fill_color(fill_color), stroke_color(stroke_color), stroke_width(stroke_width), fill_opacity(fill_opacity), SVGElement(name), transform(transform) {}
 
 // M L H V C Z
-void my_path::render(Graphics &graphics) {
-  GraphicsPath path;
-  Pen pen(stroke_color, stroke_width);
-  Color fill_colorr = Color(fill_opacity * 255, fill_color.GetR(),
-                            fill_color.GetG(), fill_color.GetB());
-  SolidBrush brush(fill_colorr);
-  path.StartFigure();
-  int control_index = 0;
-  int endPoint1 = 0;
-  int endPoint2 = 0;
-  for (int i = 0; i < commands.size(); i++) {
-    switch (commands[i]) {
-    case 'M':
-      path.StartFigure();
-      endPoint1 = points[control_index++];
-      endPoint2 = points[control_index++];
-      break;
-    case 'm':
-      path.StartFigure();
-      endPoint1 = points[control_index++];
-      endPoint2 = points[control_index++];
-      break;
-    case 'C':
-      path.AddBezier(endPoint1, endPoint2, points[control_index],
-                     points[control_index + 1], points[control_index + 2],
-                     points[control_index + 3], points[control_index + 4],
-                     points[control_index + 5]);
-      endPoint1 = points[control_index + 4];
-      endPoint2 = points[control_index + 5];
-      control_index += 6;
-      break;
-    case 'c':
-      path.AddBezier(endPoint1, endPoint2, endPoint1 + points[control_index],
-                     endPoint2 + points[control_index + 1],
-                     endPoint1 + points[control_index + 2],
-                     endPoint2 + points[control_index + 3],
-                     endPoint1 + points[control_index + 4],
-                     endPoint2 + points[control_index + 5]);
-      endPoint1 += points[control_index + 4];
-      endPoint2 += points[control_index + 5];
-      control_index += 6;
-      break;
-    case 'L':
-      path.AddLine(endPoint1, endPoint2, points[control_index],
-                   points[control_index + 1]);
-      endPoint1 = points[control_index];
-      endPoint2 = points[control_index + 1];
-      control_index += 2;
-      break;
-    case 'l':
-      path.AddLine(endPoint1, endPoint2, endPoint1 + points[control_index],
-                   endPoint2 + points[control_index + 1]);
-      endPoint1 += points[control_index];
-      endPoint2 += points[control_index + 1];
-      control_index += 2;
-      break;
-    case 'H':
-      path.AddLine(endPoint1, endPoint2, points[control_index], endPoint2);
-      endPoint1 = points[control_index];
-      control_index++;
-      break;
-    case 'h':
-      path.AddLine(endPoint1, endPoint2, endPoint1 + points[control_index],
-                   endPoint2);
-      endPoint1 += points[control_index];
-      control_index++;
-      break;
-    case 'V':
-      path.AddLine(endPoint1, endPoint2, endPoint1, points[control_index]);
-      endPoint2 = points[control_index];
-      control_index++;
-      break;
-    case 'v':
-      path.AddLine(endPoint1, endPoint2, endPoint1,
-                   endPoint2 + points[control_index]);
-      endPoint2 += points[control_index];
-      control_index++;
-      break;
-    case 'Z':
-      path.AddLine(endPoint1, endPoint2, points[0], points[1]);
-      break;
-    default:
-      break;
-    }
-  }
-  graphics.FillPath(&brush, &path);
-  graphics.DrawPath(&pen, &path);
+void my_path::render(Graphics& graphics)
+{
+	GraphicsPath path;
+	Color fill_colorr = Color(fill_opacity * 255, fill_color.GetR(), fill_color.GetG(), fill_color.GetB());
+	SolidBrush brush(fill_colorr);
+	PointF endPoint = PointF(0, 0);
+	PointF startPoint;
+	int length = d.size();
+	for (int i = 0; i < length; i++)
+	{
+		switch (get<0>(d[i]))
+		{
+		case 'M':
+			startPoint = get<1>(d[i])[0];
+			path.StartFigure();
+			endPoint = get<1>(d[i])[0];
+			if (get<1>(d[i]).size() > 1)
+			{
+				path.AddLine(endPoint, get<1>(d[i])[1]);
+				endPoint = get<1>(d[i])[1];
+			}
+			break;
+		case 'm':
+		{
+			startPoint = get<1>(d[i])[0];
+			path.StartFigure();
+			endPoint = get<1>(d[i])[0];
+			int count = 1;
+			int lengthh = get<1>(d[i]).size();
+			while (count < lengthh)
+			{
+				path.AddLine(endPoint, get<1>(d[i])[count] + endPoint);
+				endPoint = get<1>(d[i])[count] + endPoint;
+				count++;
+			}
+			break;
+		}
+		case 'C':
+			path.AddBezier(endPoint, get<1>(d[i])[0], get<1>(d[i])[1], get<1>(d[i])[2]);
+			endPoint = get<1>(d[i])[2];
+			break;
+		case 'c':
+		{
+			int lengthh = get<1>(d[i]).size();
+			for (int count = 0; count < lengthh; count += 3)
+			{
+				PointF p1 = get<1>(d[i])[count] + endPoint;
+				PointF p2 = get<1>(d[i])[count + 1] + endPoint;
+				PointF p3 = get<1>(d[i])[count + 2] + endPoint;
+				path.AddBezier(endPoint, p1, p2, p3);
+				endPoint = p3;
+			}
+			break;
+		}
+		case 'L':
+			path.AddLine(endPoint, get<1>(d[i])[0]);
+			endPoint = get<1>(d[i])[0];
+			break;
+		case 'l':
+			path.AddLine(endPoint, get<1>(d[i])[0] + endPoint);
+			endPoint = endPoint + get<1>(d[i])[0];
+			break;
+		case 'H':
+			path.AddLine(endPoint, PointF(get<1>(d[i])[0].X, endPoint.Y));
+			endPoint = PointF(get<1>(d[i])[0].X, endPoint.Y);
+			break;
+		case 'h':
+			path.AddLine(endPoint, PointF(endPoint.X + get<1>(d[i])[0].X, endPoint.Y));
+			endPoint = PointF(endPoint.X + get<1>(d[i])[0].X, endPoint.Y);
+			break;
+		case 'V':
+			path.AddLine(endPoint, PointF(endPoint.X, get<1>(d[i])[0].Y));
+			endPoint = PointF(endPoint.X, get<1>(d[i])[0].Y);
+			break;
+		case 'v':
+			path.AddLine(endPoint, PointF(endPoint.X, endPoint.Y + get<1>(d[i])[0].Y));
+			endPoint = PointF(endPoint.X, endPoint.Y + get<1>(d[i])[0].Y);
+			break;
+		case 'z':
+		case 'Z':
+			path.AddLine(endPoint, startPoint);
+			endPoint = startPoint;
+			path.CloseFigure();
+			break;
+		default:
+			break;
+		}
+	}
+	graphics.FillPath(&brush, &path);
+	if (stroke_width != 1)
+	{
+		Pen pen(stroke_color, stroke_width);
+		graphics.DrawPath(&pen, &path);
+	}
 }
 
 #endif
--- SVGRenderer.h
+++ SVGRenderer.h
@@ -1,4 +1,4 @@
-#ifndef SVGRENDERER_H
+﻿#ifndef SVGRENDERER_H
 #define SVGRENDERER_H
 
 #include "SVGElement.h"
@@ -6,30 +6,30 @@
 
 class SVGRenderer {
 private:
-  Transform transform;
-
+	Transform transform;
 public:
-  void render(Graphics &graphic, vector<SVGElement *>);
-  void renderTransform(Graphics &graphic, string transform);
+	void render(Graphics& graphic, vector<SVGElement*>);
+	void renderTransform(Graphics& graphic, string transform);
 };
 
-void SVGRenderer::render(Graphics &graphic, vector<SVGElement *> element) {
-  for (auto i : element) {
-    string transform = i->getTransform();
-    if (transform != "") {
-      this->transform.parseTransform(transform);
-      this->transform.applyTransform(graphic);
-      i->render(graphic);
-      this->transform.resetTransform(graphic);
-      continue;
-    }
-    i->render(graphic);
-  }
+void SVGRenderer::render(Graphics& graphic, vector<SVGElement*> element) {
+
+	for (auto i : element) {
+		string transform = i->getTransform();
+		if (transform != "") {
+			this->transform.parseTransform(transform);
+			this->transform.applyTransform(graphic);
+			i->render(graphic);
+			this->transform.resetTransform(graphic);
+			continue;
+		}
+		i->render(graphic);
+	}
 }
 
-void SVGRenderer::renderTransform(Graphics &graphic, string transform) {
-  this->transform.parseTransform(transform);
-  this->transform.applyTransform(graphic);
+void SVGRenderer::renderTransform(Graphics& graphic, string transform) {
+	this->transform.parseTransform(transform);
+	this->transform.applyTransform(graphic);
 }
 
 #endif
